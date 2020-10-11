0c6aa6bb6a84e06d92fe7d9e7a81ce20d7995a0b
Timothy Wrona <tjwrona1992@gmail.com>
2020-10-11 19:54:46 -0400

    Made the "dsp::Signal" class a template so multiple types can be supported for data.

--- include/tnt/dsp/signal.hpp
+++ include/tnt/dsp/signal.hpp
@@ -1,19 +1,73 @@
 #pragma once
 
-#include <memory>
+#include <vector>
 
 namespace tnt::dsp
 {
 
+// TODO: Restrict to only std::floating_point and std::complex<std::floating_point> with concepts
+template <typename T>
 class Signal
 {
 public:
-    Signal();
+    Signal(size_t sampleRate) : m_sampleRate(sampleRate) {}
+    Signal(size_t sampleRate, size_t size) : m_data(size), m_sampleRate(sampleRate) {}
+    Signal(size_t sampleRate, std::vector<T> data) : m_data(data), m_sampleRate(sampleRate) {}
+
     virtual ~Signal() = default;
 
+    size_t GetCapacity() const
+    {
+        return m_data.capacity();
+    }
+
+    size_t GetSampleRate() const
+    {
+        return m_sampleRate;
+    }
+
+    size_t GetSize() const
+    {
+        return m_date.size();
+    }
+
+    void SetCapacity(size_t capacity)
+    {
+        m_data.reserve(capacity);
+    }
+
+    void SetSampleRate(size_t sampleRate)
+    {
+        m_sampleRate = sampleRate;
+    }
+
+    void SetSize(size_t size)
+    {
+        m_data.resize(size);
+    }
+
+    const T& operator[](size_t index) const
+    {
+        return m_data[index];
+    }
+
+    T& operator[](size_t index)
+    {
+        return m_data[index];
+    }
+
+    // Forward the iterators from the underlying data type
+    typedef typename std::vector<T>::iterator iterator;
+    typedef typename std::vector<T>::const_iterator const_iterator;
+    iterator begin() { return m_data.begin(); }
+    const_iterator begin() { return m_data.begin(); }
+    const_iterator cbegin() { return m_data.cbegin(); }
+    iterator end() { return m_data.end(); }
+    const_iterator end() { return m_data.end(); }
+    const_iterator cend() { return m_data.cend(); }
 private:
-    struct Impl;
-    std::unique_ptr<Impl> m_impl;
+    std::vector<T> m_data;
+    size_t m_sampleRate;
 };
 
 } /* namespace tnt::dsp */
