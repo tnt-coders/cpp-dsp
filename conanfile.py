74b3bdcec7dbe49acf887469ed38fbc6408c6096
Timothy Wrona <tjwrona1992@gmail.com>
2021-01-03 22:58:05 -0500

    Reformatted conanfile

--- conanfile.py
+++ conanfile.py
@@ -1,19 +1,38 @@
 from conans import ConanFile, CMake, tools
 
 class CppDsp(ConanFile):
-    author = "TNT Coders <tnt-coders@googlegroups.com>"
-    build_requires = ["catch2/3.0.0-1@tnt-coders/stable",
-                      "math/1.0.0@tnt-coders/stable"]
-    default_options = {"shared": False}
-    description = "C++ Implementation of common digital signal processing algorithms"
-    exports_sources = "CMakeLists.txt", "docs/*", "include/*", "src/*", "test/*"
-    generators = "cmake", "cmake_paths"
-    license = "GNU Lesser General Public License v3.0"
     name = "dsp"
-    options = {"shared": [True, False]}
-    settings = "os", "compiler", "build_type", "arch"
-    topics = ("dsp")
+    description = "C++ Implementation of common digital signal processing algorithms"
+    homepage = "https://tnt-coders.github.io/"
     url = "https://github.com/tnt-coders/cpp-dsp"
+    license = "GNU Lesser General Public License v3.0"
+    author = "TNT Coders <tnt-coders@googlegroups.com>"
+
+    topics = ("dsp")
+
+    settings = ("os", "compiler", "build_type", "arch")
+
+    options = {
+        "shared": [True, False],
+    }
+
+    default_options = {
+        "shared": False,
+    }
+
+    build_requires = (
+        "catch2/3.0.0-1@tnt-coders/stable",
+        "math/1.0.0@tnt-coders/stable",
+    )
+
+    exports_sources = ("CMakeLists.txt", "docs/*", "include/*", "src/*", "test/*")
+
+    generators = ("cmake", "cmake_paths")
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure()
+        return cmake
 
     def configure(self):
         tools.check_min_cppstd(self, "17")
@@ -29,8 +48,3 @@ class CppDsp(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
-
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.configure()
-        return cmake
